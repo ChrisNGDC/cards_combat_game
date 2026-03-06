0989ff5bed7db023c5aba63e969cdbc1c01cba0c
Christian Gabor <chrisgabor95@gmail.com>
2026-03-06 16:22:50 -0300

    build-minor; Update version display logic to remove prefix and change version to 0.0.0

--- scripts/MainMenu/version.gd
+++ scripts/MainMenu/version.gd
@@ -4,7 +4,7 @@ extends Label
 func _ready() -> void:
 	var file = FileAccess.open("res://version.txt", FileAccess.READ)
 	if file:
-		text = "v" + file.get_as_text().strip_edges()
+		text = file.get_as_text().strip_edges()
 
 
 func _process(_delta: float) -> void:
