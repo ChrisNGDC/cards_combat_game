618e90e3d141b07806d5322cf2d0cd260442565d
Christian Gabor <chrisgabor95@gmail.com>
2026-03-08 13:41:10 -0300

    update MirrorCard instantiation in MageDeck to correct card values

--- scripts/Decks/mage_deck.gd
+++ scripts/Decks/mage_deck.gd
@@ -13,5 +13,5 @@ func crear_cartas():
 	cartas.append(MagicCard.new(0, 3))
 	cartas.append(MirrorCard.new(0, 0))
 	cartas.append(MirrorCard.new(0, 0))
-	cartas.append(MirrorCard.new(0, 2))
+	cartas.append(MirrorCard.new(0, 0))
 	cartas.append(PotionCard.new(0, 2))
